34f8a5928257798467e57c01714c4e62b846aca4
novicecoder205 <warpick2000@gmail.com>
2025-10-01 15:57:32 +0700

    Update Restaurant_Order_Analysis2.sql

--- Restaurant_Order_Analysis2.sql
+++ Restaurant_Order_Analysis2.sql
@@ -10,15 +10,25 @@
 ALTER TABLE order_details RENAME COLUMN `ï»¿order_details_id` TO `order_details_id`;
 SELECT * FROM order_details;
 -- 2. What is the data range of the table?
-SELECT *
-FROM order_details
-ORDER BY order_date;
-
 SELECT MIN(order_date), MAX(order_date) FROM order_details;
 -- 3. How many orders were made within this data range?
-
+SELECT COUNT(DISTINCT order_id) FROM order_details;
 -- 4. How many items were ordered within this data range?
-
+SELECT COUNT(*) FROM order_details;
 -- 5. Which orders had the most number of items?
-
+SELECT order_id, COUNT(item_id) AS num_items
+FROM order_details
+GROUP BY order_id
+ORDER BY num_items DESC;
 -- 6. How many orders had more than 12 items?
+SELECT order_id, COUNT(item_id) AS num_items
+FROM order_details
+GROUP BY order_id
+HAVING num_items > 12;
+
+-- 6b. Find how many rows are in each output.
+SELECT COUNT(*) FROM 
+(SELECT order_id, COUNT(item_id) AS num_items
+FROM order_details
+GROUP BY order_id
+HAVING num_items > 12) AS num_orders;
