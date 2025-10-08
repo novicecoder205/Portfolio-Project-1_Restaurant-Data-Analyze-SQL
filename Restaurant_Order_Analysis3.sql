35a9daecc0c9468079fb659a8f48f049de055014
novicecoder205 <warpick2000@gmail.com>
2025-10-08 15:02:18 +0700

    Update Restaurant_Order_Analysis3.sql

--- Restaurant_Order_Analysis3.sql
+++ Restaurant_Order_Analysis3.sql
@@ -36,5 +36,9 @@ WHERE order_id = 440
 GROUP BY category;
 
 -- 5.View details of top 5 highest spend order.
-
+SELECT order_id, category, COUNT(item_id) AS num_items
+FROM order_details od LEFT JOIN menu_items mi
+     ON od.item_id =  mi.menu_item_id
+WHERE order_id IN (440, 2075, 1957, 330, 2675)
+GROUP BY order_id, category;
 
